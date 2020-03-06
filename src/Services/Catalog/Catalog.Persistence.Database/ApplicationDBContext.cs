69609d83eecb62a562b7f2f914bcc007e5d8452a
SergioRomero96 <sergio.nicolas777@gmail.com>
2020-03-06 15:39:25 -0300

    Entity Framework Core: DbContext

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(
+            DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<productInStock> Stocks { get; set; }
+    }
+}
