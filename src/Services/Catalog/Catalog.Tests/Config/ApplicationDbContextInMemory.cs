c56cdc63fc87ec67ebba056ccdf85f58fcb391e4
SergioRomero96 <sergio.nicolas777@gmail.com>
2020-03-15 12:50:47 -0300

    UNIT TESTING - PARTE I (Configurando el Entity Framework Core para que trabaje con una base de datos en Memoria.)

---
+++ src/Services/Catalog/Catalog.Tests/Config/ApplicationDbContextInMemory.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Tests.Config
+{
+    public static class ApplicationDbContextInMemory
+    {
+        public static ApplicationDBContext Get()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: $"Catalog.Db")
+                .Options;
+
+            return new ApplicationDBContext(options);
+        }
+    }
+}
